9a2618f050f52b169d5b2c59bf93668c089c71e6
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-04-02 14:21:27 +0600

    open

---
+++ MM Purchase/Serecns/Purchase/Controller/PurchaseVC.swift
@@ -0,0 +1,29 @@
+//
+//  PurchaseVC.swift
+//  MM Purchase
+//
+//  Created by Shishir_Mac on 2/4/23.
+//
+
+import UIKit
+
+class PurchaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
